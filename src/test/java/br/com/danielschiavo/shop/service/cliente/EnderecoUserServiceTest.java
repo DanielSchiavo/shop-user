39b6d7c6de2888e72ecfb9cff7fb65861cc4e8fc
Daniel Schiavo Rosseto <daniel.schiavo35@gmail.com>
2024-05-08 23:33:56 -0300

    Refatorado testes para se adequar as mudanças de relacionamento de Cliente para Endereço

--- src/test/java/br/com/danielschiavo/shop/service/cliente/EnderecoUserServiceTest.java
+++ src/test/java/br/com/danielschiavo/shop/service/cliente/EnderecoUserServiceTest.java
@@ -70,11 +70,9 @@ class EnderecoUserServiceTest {
 	void deletarEnderecoPorIdToken_idEnderecoExiste_NaoDeveLancarExcecao() {
 		//ARRANGE
 		Endereco endereco = enderecoBuilder.id(1L).cep("29142298").rua("Divinopolis").numero("15").complemento("Sem complemento").bairro("Bela vista").cidade("Cariacica").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Endereco endereco2 = enderecoBuilder.id(2L).cep("29152291").rua("Avenida luciano das neves").numero("3233").complemento("Apartamento 302").bairro("Praia de itaparica").cidade("Vila velha").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").adicionarEndereco(endereco).adicionarEndereco(endereco2).getCliente();
 		BDDMockito.when(usuarioAutenticadoService.getCliente()).thenReturn(cliente);
 		Long idEndereco = 2L;
-		BDDMockito.when(enderecoRepository.findByIdAndCliente(idEndereco, cliente)).thenReturn(Optional.of(endereco2));
+		BDDMockito.when(enderecoRepository.findByIdAndCliente(idEndereco, cliente)).thenReturn(Optional.of(endereco));
 		
 		//ACT
 		enderecoUserService.deletarEnderecoPorIdToken(idEndereco);
@@ -87,13 +85,11 @@ class EnderecoUserServiceTest {
 	@DisplayName("Deletar enedereco por id token deve lançar exceção quando id endereco fornecido não existe")
 	void deletarEnderecoPorIdToken_idEnderecoNaoExiste_DeveLancarExcecao() {
 		//ARRANGE
-		Endereco endereco = enderecoBuilder.id(1L).cep("29142298").rua("Divinopolis").numero("15").complemento("Sem complemento").bairro("Bela vista").cidade("Cariacica").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Endereco endereco2 = enderecoBuilder.id(2L).cep("29152291").rua("Avenida luciano das neves").numero("3233").complemento("Apartamento 302").bairro("Praia de itaparica").cidade("Vila velha").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").adicionarEndereco(endereco).adicionarEndereco(endereco2).getCliente();
 		BDDMockito.when(usuarioAutenticadoService.getCliente()).thenReturn(cliente);
+		Long idEndereco = 3L;
+		BDDMockito.when(enderecoRepository.findByIdAndCliente(idEndereco, cliente)).thenReturn(Optional.empty());
 		
 		//ASSERT + ACT
-		Long idEndereco = 3L;
 		Assertions.assertThrows(ValidacaoException.class, () -> enderecoUserService.deletarEnderecoPorIdToken(idEndereco));
 	}
 	
@@ -101,28 +97,29 @@ class EnderecoUserServiceTest {
 	@DisplayName("Pegar enderecos cliente por id token deve funcionar normalmente quando cliente tem enderecos cadastrados")
 	void pegarEnderecosClientePorIdToken_ClienteTemEnderecosCadastrados_NaoDeveLancarExcecao() {
 		//ARRANGE
+		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").getCliente();
 		Endereco endereco = enderecoBuilder.id(1L).cep("29142298").rua("Divinopolis").numero("15").complemento("Sem complemento").bairro("Bela vista").cidade("Cariacica").estado("ES").enderecoPadrao(true).cliente(cliente).build();
 		Endereco endereco2 = enderecoBuilder.id(2L).cep("29152291").rua("Avenida luciano das neves").numero("3233").complemento("Apartamento 302").bairro("Praia de itaparica").cidade("Vila velha").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").adicionarEndereco(endereco).adicionarEndereco(endereco2).getCliente();
-		BDDMockito.when(enderecoRepository.findAllByCliente(cliente)).thenReturn(new ArrayList<>(List.of(endereco, endereco2)));
+		List<Endereco> listaEndereco = new ArrayList<>(List.of(endereco, endereco2));
+		BDDMockito.when(enderecoRepository.findAllByCliente(cliente)).thenReturn(listaEndereco);
 		BDDMockito.when(usuarioAutenticadoService.getCliente()).thenReturn(cliente);
 		
 		//ACT
 		List<MostrarEnderecoDTO> listaMostrarEnderecoDTO = enderecoUserService.pegarEnderecosClientePorIdToken();
 		
 		//ASSERT
-		Assertions.assertEquals(cliente.getEnderecos().size(), listaMostrarEnderecoDTO.size());
+		Assertions.assertEquals(listaEndereco.size(), listaMostrarEnderecoDTO.size());
 		for (int i = 0; i < listaMostrarEnderecoDTO.size(); i++) {
 		    MostrarEnderecoDTO enderecoResultado = listaMostrarEnderecoDTO.get(i);
 
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getCep(), enderecoResultado.cep(), "O CEP do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getRua(), enderecoResultado.rua(), "A rua do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getNumero(), enderecoResultado.numero(), "O número do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getComplemento(), enderecoResultado.complemento(), "O complemento do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getBairro(), enderecoResultado.bairro(), "O bairro do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getCidade(), enderecoResultado.cidade(), "A cidade do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getEstado(), enderecoResultado.estado(), "O estado do endereço deve ser igual");
-		    Assertions.assertEquals(cliente.getEnderecos().get(i).getEnderecoPadrao(), enderecoResultado.enderecoPadrao(), "A indicação de endereço padrão deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getCep(), enderecoResultado.cep(), "O CEP do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getRua(), enderecoResultado.rua(), "A rua do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getNumero(), enderecoResultado.numero(), "O número do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getComplemento(), enderecoResultado.complemento(), "O complemento do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getBairro(), enderecoResultado.bairro(), "O bairro do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getCidade(), enderecoResultado.cidade(), "A cidade do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getEstado(), enderecoResultado.estado(), "O estado do endereço deve ser igual");
+		    Assertions.assertEquals(listaEndereco.get(i).getEnderecoPadrao(), enderecoResultado.enderecoPadrao(), "A indicação de endereço padrão deve ser igual");
 		}
 	}
 	
@@ -141,9 +138,9 @@ class EnderecoUserServiceTest {
 	@DisplayName("Cadastrar novo endereço por id token deve executar normalmente quando enviado CadastrarEnderecoDTO correto")
 	void cadastrarNovoEnderecoPorIdToken() {
 		//ARRANGE
+		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").getCliente();
 		Endereco endereco = enderecoBuilder.id(1L).cep("29142298").rua("Divinopolis").numero("15").complemento("Sem complemento").bairro("Bela vista").cidade("Cariacica").estado("ES").enderecoPadrao(true).cliente(cliente).build();
 		Endereco endereco2 = enderecoBuilder.id(2L).cep("29152291").rua("Avenida luciano das neves").numero("3233").complemento("Apartamento 302").bairro("Praia de itaparica").cidade("Vila velha").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").adicionarEndereco(endereco).adicionarEndereco(endereco2).getCliente();
 		BDDMockito.when(usuarioAutenticadoService.getCliente()).thenReturn(cliente);
 		BDDMockito.when(enderecoRepository.findAllByCliente(cliente)).thenReturn(new ArrayList<>(List.of(endereco, endereco2)));
 		
@@ -167,25 +164,26 @@ class EnderecoUserServiceTest {
 	@DisplayName("Alterar endereco por id token deve executar normalmente quando id fornecido existe, é do cliente e AlterarEnderecoDTO é válido")
 	void alterarEnderecoPorIdToken() {
 		//ARRANGE
+		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").getCliente();
 		Endereco endereco = enderecoBuilder.id(1L).cep("29142298").rua("Divinopolis").numero("15").complemento("Sem complemento").bairro("Bela vista").cidade("Cariacica").estado("ES").enderecoPadrao(true).cliente(cliente).build();
 		Endereco endereco2 = enderecoBuilder.id(2L).cep("29152291").rua("Avenida luciano das neves").numero("3233").complemento("Apartamento 302").bairro("Praia de itaparica").cidade("Vila velha").estado("ES").enderecoPadrao(true).cliente(cliente).build();
-		Cliente cliente = clienteBuilder.id(1L).cpf("12345678994").nome("Silvana").sobrenome("Pereira da silva").dataNascimento(LocalDate.of(2000, 3, 3)).dataCriacaoConta(LocalDate.now()).email("silvana.dasilva@gmail.com").senha("{noop}123456").celular("27999833653").fotoPerfil("Qualquerfoto.jpeg").adicionarEndereco(endereco).adicionarEndereco(endereco2).getCliente();
 		BDDMockito.when(usuarioAutenticadoService.getCliente()).thenReturn(cliente);
 		Long idEnderecoASerAlterado = 1L;
-		BDDMockito.when(enderecoRepository.findAllByCliente(cliente)).thenReturn(new ArrayList<>(List.of(endereco, endereco2)));
+		List<Endereco> listaEndereco = new ArrayList<>(List.of(endereco, endereco2));
+		BDDMockito.when(enderecoRepository.findAllByCliente(cliente)).thenReturn(listaEndereco);
 		
 		//ACT
 		AlterarEnderecoDTO alterarEnderecoDTO = alterarEnderecoDTOBuilder.cep("12345678").rua("Itapua").numero("35").complemento("com complemento").bairro("aquele bairro").cidade("serra").estado("ES").enderecoPadrao(true).build();
 		MostrarEnderecoDTO mostrarEnderecoDTO = enderecoUserService.alterarEnderecoPorIdToken(alterarEnderecoDTO, idEnderecoASerAlterado);
 		
 		//ASSERT
-		Assertions.assertNotEquals(cliente.getEnderecos().get(0).getCep(), mostrarEnderecoDTO.cep());
-		Assertions.assertNotEquals(cliente.getEnderecos().get(0).getRua(), mostrarEnderecoDTO.rua());
-		Assertions.assertNotEquals(cliente.getEnderecos().get(0).getNumero(), mostrarEnderecoDTO.numero());
-		Assertions.assertNotEquals(cliente.getEnderecos().get(0).getComplemento(), mostrarEnderecoDTO.complemento());
-		Assertions.assertNotEquals(cliente.getEnderecos().get(0).getBairro(), mostrarEnderecoDTO.bairro());
-		Assertions.assertNotEquals(cliente.getEnderecos().get(0).getCidade(), mostrarEnderecoDTO.cidade());
-		Assertions.assertEquals(cliente.getEnderecos().get(0).getEstado(), mostrarEnderecoDTO.estado());
-		Assertions.assertEquals(cliente.getEnderecos().get(0).getEnderecoPadrao(), mostrarEnderecoDTO.enderecoPadrao());
+		Assertions.assertNotEquals(listaEndereco.get(0).getCep(), mostrarEnderecoDTO.cep());
+		Assertions.assertNotEquals(listaEndereco.get(0).getRua(), mostrarEnderecoDTO.rua());
+		Assertions.assertNotEquals(listaEndereco.get(0).getNumero(), mostrarEnderecoDTO.numero());
+		Assertions.assertNotEquals(listaEndereco.get(0).getComplemento(), mostrarEnderecoDTO.complemento());
+		Assertions.assertNotEquals(listaEndereco.get(0).getBairro(), mostrarEnderecoDTO.bairro());
+		Assertions.assertNotEquals(listaEndereco.get(0).getCidade(), mostrarEnderecoDTO.cidade());
+		Assertions.assertEquals(listaEndereco.get(0).getEstado(), mostrarEnderecoDTO.estado());
+		Assertions.assertEquals(listaEndereco.get(0).getEnderecoPadrao(), mostrarEnderecoDTO.enderecoPadrao());
 	}
 }
