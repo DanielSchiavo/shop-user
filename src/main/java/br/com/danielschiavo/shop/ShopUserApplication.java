4ee8344ea299725beadbb2b276a54adf9b59a943
Daniel Schiavo Rosseto <daniel.schiavo35@gmail.com>
2024-05-30 14:23:14 -0300

    Alterado pom.xml colocando como pai o projeto raiz e agora arquivos .properties são mesclados com os arquivos .properties do submodulo shop-compartilhado

--- src/main/java/br/com/danielschiavo/shop/ShopUserApplication.java
+++ src/main/java/br/com/danielschiavo/shop/ShopUserApplication.java
@@ -1,20 +1,22 @@
 package br.com.danielschiavo.shop;
 
-import org.springframework.boot.SpringApplication;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "br.com.danielschiavo")
 @ComponentScan(basePackages = "br.com.danielschiavo")
 @EntityScan("br.com.danielschiavo.shop.model")
-@PropertySource("classpath:application-${spring.profiles.active}.properties")
 @EnableJpaRepositories("br.com.danielschiavo")
 public class ShopUserApplication extends SpringBootServletInitializer {
 	
@@ -23,8 +25,22 @@ public class ShopUserApplication extends SpringBootServletInitializer {
         return application.sources(ShopUserApplication.class);
     }
 	
-	public static void main(String[] args) {
-		SpringApplication.run(ShopUserApplication.class, args);
+	public static void main(String[] args) throws IOException {
+	
+		
+        org.springframework.core.io.Resource resource = new ClassPathResource("application.properties");
+        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+        String activeProfile = props.getProperty("spring.profiles.active");
+        String serverPort = props.getProperty("shopuser.server.port");
+        
+        System.out.println(" PORTIS " + serverPort);
+        
+        String additionalConfig = "classpath:/application-" + activeProfile + "-compartilhado.properties";
+        
+        new SpringApplicationBuilder(ShopUserApplication.class)
+                .properties("spring.config.additional-location=" + additionalConfig)
+                .properties("server.port=" + serverPort)
+                .run(args);
 	}
 
 }
